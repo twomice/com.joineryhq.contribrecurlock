b20b734c0f26d4827c9f361bf641e59ea3b24426
Allen Shaw <allen@JoineryHQ.com>
2018-02-09 11:01:54 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+#Lock recurring contributions#
+
+In a nonprofit context, allowing the end user to edit and/or cancel their own recurring
+contributions is generally desirable. In a fee-for-services environment, this is
+not always true; it can be desirable to require the end user to update their
+recurring payments through other channels.
+
+This extension changes the required permissions for the recurring contribution 
+"update" and "cancel" forms. CiviCRM core requires "make online contributions" for
+these forms. This extension causes these forms to require both CiviCRM permissions 
+"access CiviContribute" and "edit contributions."
---
+++ contribrecurlock.php
@@ -0,0 +1,152 @@
+<?php
+
+require_once 'contribrecurlock.civix.php';
+use CRM_Contribrecurlock_ExtensionUtil as E;
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function contribrecurlock_civicrm_config(&$config) {
+  _contribrecurlock_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function contribrecurlock_civicrm_xmlMenu(&$files) {
+  _contribrecurlock_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function contribrecurlock_civicrm_install() {
+  _contribrecurlock_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+ */
+function contribrecurlock_civicrm_postInstall() {
+  _contribrecurlock_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function contribrecurlock_civicrm_uninstall() {
+  _contribrecurlock_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function contribrecurlock_civicrm_enable() {
+  _contribrecurlock_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function contribrecurlock_civicrm_disable() {
+  _contribrecurlock_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function contribrecurlock_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _contribrecurlock_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function contribrecurlock_civicrm_managed(&$entities) {
+  _contribrecurlock_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function contribrecurlock_civicrm_caseTypes(&$caseTypes) {
+  _contribrecurlock_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+ */
+function contribrecurlock_civicrm_angularModules(&$angularModules) {
+  _contribrecurlock_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function contribrecurlock_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _contribrecurlock_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
+ *
+function contribrecurlock_civicrm_preProcess($formName, &$form) {
+
+} // */
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_navigationMenu
+ *
+function contribrecurlock_civicrm_navigationMenu(&$menu) {
+  _contribrecurlock_civix_insert_navigation_menu($menu, NULL, array(
+    'label' => E::ts('The Page'),
+    'name' => 'the_page',
+    'url' => 'civicrm/the-page',
+    'permission' => 'access CiviReport,access CiviContribute',
+    'operator' => 'OR',
+    'separator' => 0,
+  ));
+  _contribrecurlock_civix_navigationMenu($menu);
+} // */
