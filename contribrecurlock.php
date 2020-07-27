88f640f1e3bf9e472b4e842714b97c8db848011e
lucky091588 <lucky091588@gmail.com>
2020-07-28 01:26:29 +0800

    Uncomment warning on unused use statement

--- contribrecurlock.php
+++ contribrecurlock.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'contribrecurlock.civix.php';
-// use CRM_Contribrecurlock_ExtensionUtil as E;
+use CRM_Contribrecurlock_ExtensionUtil as E;
 
 /**
  * Implements hook_civicrm_config().
